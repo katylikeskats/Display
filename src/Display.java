1ba84e43ab4685c1cd39ab262c6a8ef7bbe6ed07
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-18 19:25:21 -0400

    Established JFrame

--- src/Display.java
+++ src/Display.java
@@ -1,10 +1,38 @@
-public class Display {
-    private Bracket tournament;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
-    Display(Bracket tournament){
-        this.tournament = tournament;
-    }
+public class Display extends JFrame{
+    //private Bracket tournament;
+    private int maxX;
+    private int maxY;
 
-    //hi
-    public void update(Bracket tournament);//hi
+    //Display(Bracket tournament){
+    Display(){
+        super();
+        //this.tournament = tournament;
+
+        maxX = 1530;
+        maxY = 1150;
+        this.setSize(maxX, maxY);
+        this.setLocationRelativeTo(null); //start the frame in the center of the screen
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setUndecorated(true);
+
+
+        //Creating a frame for the black background (otherwise would interfere with other layers if each panel had a black background)
+        JPanel blackBackground = new JPanel();
+        blackBackground.setSize(new Dimension(maxX, maxY));
+        blackBackground.setBackground(new Color(1,1,1));
+        blackBackground.setLocation(0,0);
+
+        this.setVisible(true);
+        }
+
+
+   // public void update(Bracket tournament);
+
 }
