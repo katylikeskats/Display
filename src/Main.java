6dadecb67faae6947a0d788eea5b61c89815a343
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-19 11:31:10 -0400

    woo debugged

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
         String[][][][] array = {{{{"A"}, {"B"}}, {{"C"}, {"D"}}, {{"E"}, {"F"}}, {{"G", "H"}}},
                 {{{"A", "B"}, {"C", "D"}}, {{"E", "F"}, {"G", "H"}}},
-        {{{"A", "B", "C", "D"}}, {{"E", "F", "G", "H"}}}};
+                {{{"A", "B", "C","D"}, {"E", "F","G","H"}}}};
 
         Bracket tournament= new Bracket(8, array);
         new Display(tournament);
