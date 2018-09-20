e2b0c666ba183171f758898051518a63d448da5a
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-20 09:54:15 -0400

    more commenting

--- src/TournamentPanel.java
+++ src/TournamentPanel.java
@@ -1,3 +1,10 @@
+/**
+ * [TournamentPanel.java]
+ * Abstract tournament panel class
+ * @author Katelyn Wang & Dora Su
+ * September 20 2018 
+ */
+
 import javax.swing.JPanel;
 
 abstract public class TournamentPanel extends JPanel {
