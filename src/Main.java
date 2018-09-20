f5571b6939cd12332c5eedeb1aa6157525a780bf
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-20 11:52:14 -0400

    fdksl

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         Bracket tournament= new Bracket(16, array2);
         Display display = new Display(tournament);
-        //Thread.sleep(2000);//small delay
-        //display.update(new Bracket(8, array1));
+        Thread.sleep(2000);//small delay
+        display.update(new Bracket(8, array1));
     }
 }
