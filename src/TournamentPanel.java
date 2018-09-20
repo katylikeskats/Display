d724daf39c250b101ae2743350000f140d9241d7
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-20 00:14:29 -0400

    commenting

--- src/TournamentPanel.java
+++ src/TournamentPanel.java
@@ -111,7 +111,10 @@ public class TournamentPanel extends JPanel {
         }
     }
 
-
+    /**
+     * Updates the tournament bracket display by changing the tournament bracket it is drawing. Repaints the screen once called
+     * @param tournament Updated tournament bracket to be displayed
+     */
     public void setTournament(Bracket tournament) {
         this.tournament = tournament;
         repaint();
