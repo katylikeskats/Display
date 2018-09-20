de7d78eed3e7c5cbc0d88c8d270daef6d38ee324
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-20 00:12:53 -0400

    delay edited

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         Bracket tournament= new Bracket(8, array1);
         Display display = new Display(tournament);
-        Thread.sleep(500);
+        Thread.sleep(2000);//small delay 
         display.update(new Bracket(16, array2));
     }
 }
