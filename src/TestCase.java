e813ec199632d03e1a8a1c79e356cdb514c7e75e
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-21 11:14:45 -0400

    fjkd

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+
 public class TestCase {
+    public static void main(String[] args){
+        ArrayList<Team> teams = new ArrayList<Team>();
+        SingleGenerator generator;
+
+        for (int i = 1; i < 8; i++) {
+            teams.add(new Team(Integer.toString(i), i));
+        }
+
+        generator = new SingleGenerator(teams, true);
+
+    }
 
 }
