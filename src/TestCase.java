f857db30eb4c0bcdcf09578d365d94d253f09d98
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-27 00:14:45 -0400

    tested javadoc generation- successful!

--- src/TestCase.java
+++ src/TestCase.java
@@ -5,7 +5,7 @@ public class TestCase {
         ArrayList<Team> teams = new ArrayList<Team>();
         SingleGenerator generator;
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 1100; i++) {
             teams.add(new Team(Integer.toString(i), i));
         }
 
