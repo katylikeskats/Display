a08127d5d3fe102255663365e668befc33233401
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-09-30 19:40:57 -0400

    added double bracket stuff

--- src/TestCase.java
+++ src/TestCase.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class TestCase {
     public static void main(String[] args){
         ArrayList<Team> teams = new ArrayList<Team>();
-        SingleGenerator generator;
+        DoubleGenerator generator;
 
         for (int i = 1; i <= 567; i++) {
             teams.add(new Team(Integer.toString(i), i));
         }
 
-        generator = new SingleGenerator(teams, true);
+        generator = new DoubleGenerator(teams);
 
         Bracket bracket = generator.getBracket();
 
