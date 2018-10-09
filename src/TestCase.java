35534007496b08e9d46cd9425254fb4e3a795057
katylikeskats <39709286+katylikeskats@users.noreply.github.com>
2018-10-08 23:38:48 -0400

    updated Yili's files

--- src/TestCase.java
+++ src/TestCase.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 public class TestCase {
     public static void main(String[] args){
         ArrayList<Team> teams = new ArrayList<Team>();
-        //SingleGenerator generator1;
-        DoubleGenerator generator2;
+        SingleGenerator generator1;
+       // DoubleGenerator generator2;
 
         for (int i = 1; i <= 19; i++) {
             teams.add(new Team(Integer.toString(i), i));
         }
 
-       // generator1 = new SingleGenerator(teams, true);
-        generator2 = new DoubleGenerator(teams);
+        generator1 = new SingleGenerator(teams, true);
+       // generator2 = new DoubleGenerator(teams);
 
-        Bracket bracket = generator2.getBracket();
+        Bracket bracket = generator1.getBracket();
 
         //new ManagementSystem();
-        Display display = new Display(generator2.getBracket());
+        Display display = new Display(generator1.getBracket());
         //Display display2 = new Display(generator2.getBracket());
-        bracket.setMatchWinner("1", 1, 1);
+        bracket.setMatchWinner("17", 1, 1);
+        bracket.setMatchWinner("1", 2, 1);
         bracket.setMatchWinner("19", 1, 3);
-        bracket.setMatchWinner("19", 3, 2);
         //bracket.setMatchWinner("2", 1, 3);
         display.update(bracket);
     }
